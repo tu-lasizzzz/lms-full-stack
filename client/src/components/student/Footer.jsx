988c474c93f2da72893097608a0d36a3a7757881
Tulasi Sahu <tulasisahu20@gmail.com>
2026-05-22 02:09:35 +0530

    upd jsx

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className="flex flex-col md:items-start items-center w-full">
           <img src={assets.logo_dark} alt="logo" />
           <p className="mt-6 text-center md:text-left text-sm text-white/80">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.
+            Not just courses — Edemy is where skills meet ambition, helping the next generation learn, build, and level up.
           </p>
         </div>
 
@@ -36,7 +36,7 @@ const Footer = () => {
 
       </div>
       <p className="py-4 text-center text-xs md:text-sm text-white/60">
-        Copyright 2024 © GreatStack. All Right Reserved.
+        © 2026 Edemy. Turning learners into future creators.
       </p>
     </footer>
   );
